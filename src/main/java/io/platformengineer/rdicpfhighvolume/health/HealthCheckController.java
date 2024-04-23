14f706f9ab9c6c71ff6def8bf742118b1d5e7b1e
gacerioni <gacerioni@gmail.com>
2024-04-23 18:34:02 -0300

    0.4.0-GABS

---
+++ src/main/java/io/platformengineer/rdicpfhighvolume/health/HealthCheckController.java
@@ -0,0 +1,27 @@
+package io.platformengineer.rdicpfhighvolume.health;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.platformengineer.rdicpfhighvolume.person.PersonService;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public ResponseEntity<String> checkDatabaseHealth() {
+        if (personService.isDatabaseConnectionAlive()) {
+            return ResponseEntity.ok("Database is up!");
+        } else {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Database is down!");
+        }
+    }
+}
